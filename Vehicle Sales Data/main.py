ed1c4e0d85f601617c0f75520306db3939ac37b6
Logan <loganwinfree4@gmail.com>
2026-06-11 12:48:39 -0400

    Added distribution graphs for numerical variables: Condition and Odometer.

--- Vehicle Sales Data/main.py
+++ Vehicle Sales Data/main.py
@@ -16,11 +16,22 @@ X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.
 # Combine training data for EDA
 train_data = pd.concat([X_train, y_train], axis=1)
 
-
-
+# Interval - Years
+plt.figure("Distribution of Vehicle Years")
 plt.hist(train_data['year'], bins=np.unique(train_data['year']))
 plt.title("Distribution of Vehicle Years")
 plt.xlabel("Year")
-plt.xticks(np.arange(np.unique(train_data['year']).min(), np.unique(train_data['year']).max()+1, 1), rotation=-45)
+#plt.xticks(np.arange(np.unique(train_data['year']).min(), np.unique(train_data['year']).max()+1, 1), rotation=-45)
+
+# Numerical
+fig, (ax1, ax2) = plt.subplots(1,2)
+fig.canvas.manager.set_window_title("Distributions of Numerical Variables")
+ax1.boxplot(train_data['condition'].dropna())
+ax1.set_ylim(0,100)
+ax1.set_xlabel("Condition")
+ax2.boxplot(train_data['odometer'].dropna(), label="Odometer")
+ax2.set_xlabel("Odometer")
+
+
 
 plt.show()
